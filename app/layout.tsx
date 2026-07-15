359d9b77688f162600b2f98d966da1f29e9f2e91
mohammedrashad2011-ui <mohammed.rashad2011@gmail.com>
2026-07-15 12:50:24 +0300

    Meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 const alexandria = Alexandria({ variable: "--font-arabic", subsets: ["arabic", "latin"] });
 const cormorant = Cormorant_Garamond({ variable: "--font-display", subsets: ["latin"], weight: ["600", "700"] });
 const gaId = "G-5SKKT1NBHB";
+const metaPixelId = "2274266710087331";
 
 export const metadata: Metadata = {
   title: "ديوانك | براندينج، تسويق وأتمتة ذكاء اصطناعي",
@@ -35,7 +36,32 @@ export default function RootLayout({
           gtag('config', '${gaId}');
         `}
       </Script>
+
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${metaPixelId}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
       <body className={`${alexandria.variable} ${cormorant.variable}`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
